bc242bfdcdf3ec257241569f32cf1f895f5576d3
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-06-06 16:19:39 +0700

    Apply gol as background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 //@ts-expect-error Gol is jsx
 import Gol from './Gol'
+import Portofolio from './Portofolio'
 
 export default function App() {
   return (
     <div>
-      <Gol />
+      <Portofolio />
+      <div className="absolute left-0 top-0 z-[-1]">
+        <Gol />
+      </div>
     </div>
   )
 }
---
+++ src/Portofolio.tsx
@@ -0,0 +1,3 @@
+export default function Portofolio() {
+  return <div>Portofolio</div>
+}
