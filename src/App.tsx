cd57f7be0a06f61d0016c072bc1b343b127aab68
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-06-06 23:04:03 +0700

    Add cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 //@ts-expect-error Gol is jsx
 import Gol from './Gol'
 import Portofolio from './Portofolio'
+import Cursor from './Cursor'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <div className="fixed left-0 top-0 z-[-1]">
         <Gol />
       </div>
+      <Cursor />
     </div>
   )
 }
---
+++ src/Cursor.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+
+export default function Cursor() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
+  useEffect(() => {
+    const updateMousePosition = (ev: any) => {
+      setMousePosition({ x: ev.clientX, y: ev.clientY })
+    }
+    window.addEventListener('mousemove', updateMousePosition)
+    return () => {
+      window.removeEventListener('mousemove', updateMousePosition)
+    }
+  }, [])
+
+  return (
+    <div
+      className="w-6 h-6 bg-[#aaaaaa] rounded-full fixed z-[-1] pointer-events-none"
+      style={{ left: mousePosition.x - 12, top: mousePosition.y - 12 }}
+    ></div>
+  )
+}
