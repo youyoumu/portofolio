78b53d2afb7c56b0cfcb421ca8958bf6dcacd9d7
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-06-06 20:34:06 +0700

    Style about me

--- src/Portofolio.tsx
+++ src/Portofolio.tsx
@@ -1,8 +1,12 @@
 export default function Portofolio() {
   return (
     <div className="min-w-screen max-w-screen font-outfit">
-      <div className="h-screen w-full flex justify-center items-center text-4xl p-4 drop-shadow">
-        Hello I'm youyoumu, self taught fullstack web developer.
+      <div className="h-screen w-full flex justify-center items-center text-5xl p-4 drop-shadow">
+        <p className="max-w-2xl leading-snug">
+          Hey, I'm <span className="underline font-medium">youyoumu</span>, a
+          self taught fullstack web developer from{' '}
+          <span className="underline">Indonesia</span>.
+        </p>
       </div>
     </div>
   )
