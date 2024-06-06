5c354e79b3eb5764b6479aee4bd95d2bd6f10a41
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-06-06 17:01:03 +0700

    Add font

--- src/Portofolio.tsx
+++ src/Portofolio.tsx
@@ -1,3 +1,9 @@
 export default function Portofolio() {
-  return <div>Portofolio</div>
+  return (
+    <div className="min-w-screen max-w-screen font-outfit">
+      <div className="h-screen w-full flex justify-center items-center text-4xl p-4 drop-shadow">
+        Hello I'm youyoumu, self taught fullstack web developer.
+      </div>
+    </div>
+  )
 }
