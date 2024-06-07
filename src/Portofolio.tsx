c5b03fbaa762b44d780b2e4ccb0ae9b6f7837e3e
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-06-07 11:00:01 +0700

    Remove dark mode

--- src/Portofolio.tsx
+++ src/Portofolio.tsx
@@ -14,7 +14,7 @@ export default function Portofolio() {
         text="HTML CSS JavaScript TypeScript Ruby SQL Linux Bash Git "
         text2="Next.js Ruby on Rails React Tailwind CSS"
         default_velocity={3}
-        className="font-display text-center text-4xl font-medium tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
+        className="font-display text-center text-4xl font-medium tracking-[-0.02em] text-black drop-shadow-sm md:text-7xl md:leading-[5rem]"
       />
     </div>
   )
